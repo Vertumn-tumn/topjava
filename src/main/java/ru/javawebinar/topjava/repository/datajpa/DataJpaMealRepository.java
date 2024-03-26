a8d66d20512974a89559f0db13b1f1ea6e0fdcb0
evgen <vertumnartist@bk.ru>
2024-03-26 18:33:27 +0300

    implemented DataJpaMealRepository and tested by MealServiceTest

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
@@ -11,33 +12,39 @@ import java.util.List;
 public class DataJpaMealRepository implements MealRepository {
 
     private final CrudMealRepository crudRepository;
+    private final CrudUserRepository crudUserRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
+    public DataJpaMealRepository(CrudMealRepository crudRepository, CrudUserRepository crudUserRepository) {
         this.crudRepository = crudRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        User user = crudUserRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Пользователь с id " + userId + " не найден"));
+        meal.setUser(user);
+        if (meal.isNew()) return crudRepository.save(meal);
+        return get(meal.getId(), userId) == null ? null : crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.get(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.getAll(userId);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudRepository.getBetween(startDateTime, endDateTime, userId);
     }
 }
