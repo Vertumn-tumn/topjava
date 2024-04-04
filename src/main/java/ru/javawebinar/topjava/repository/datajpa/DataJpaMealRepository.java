f0cf7e329b51210cfedd5eb5347fbe941091f943
evgen <vertumnartist@bk.ru>
2024-04-04 16:59:42 +0300

    6_06_HW5_optional_fetch_join.patch

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -16,39 +16,44 @@ public class DataJpaMealRepository implements MealRepository {
     private final CrudUserRepository crudUserRepository;
 
     public DataJpaMealRepository(CrudMealRepository crudMealRepository, CrudUserRepository crudUserRepository) {
-            this.crudMealRepository = crudMealRepository;
-            this.crudUserRepository = crudUserRepository;
-        }
-
-        @Override
-        @Transactional
-        public Meal save(Meal meal, int userId) {
-            if (!meal.isNew() && get(meal.id(), userId) == null) {
-                return null;
-            }
-            meal.setUser(crudUserRepository.getReferenceById(userId));
-            return crudMealRepository.save(meal);
-        }
-
-        @Override
-        public boolean delete(int id, int userId) {
-            return crudMealRepository.delete(id, userId) != 0;
-        }
-
-        @Override
-        public Meal get(int id, int userId) {
-            return crudMealRepository.findById(id)
-                    .filter(meal -> meal.getUser().getId() == userId)
-                    .orElse(null);
-        }
-
-        @Override
-        public List<Meal> getAll(int userId) {
-            return crudMealRepository.getAll(userId);
-        }
-
-        @Override
-        public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-            return crudMealRepository.getBetweenHalfOpen(startDateTime, endDateTime, userId);
-        }
+        this.crudMealRepository = crudMealRepository;
+        this.crudUserRepository = crudUserRepository;
     }
+
+    @Override
+    @Transactional
+    public Meal save(Meal meal, int userId) {
+        if (!meal.isNew() && get(meal.id(), userId) == null) {
+            return null;
+        }
+        meal.setUser(crudUserRepository.getReferenceById(userId));
+        return crudMealRepository.save(meal);
+    }
+
+    @Override
+    public boolean delete(int id, int userId) {
+        return crudMealRepository.delete(id, userId) != 0;
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        return crudMealRepository.findById(id)
+                .filter(meal -> meal.getUser().getId() == userId)
+                .orElse(null);
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return crudMealRepository.getAll(userId);
+    }
+
+    @Override
+    public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        return crudMealRepository.getBetweenHalfOpen(startDateTime, endDateTime, userId);
+    }
+
+    @Override
+    public Meal getWithUser(int id, int userId) {
+        return crudMealRepository.getWithUser(id, userId);
+    }
+}
