91d441a73899f23546806e68ec37532f96f4822a
evgen <vertumnartist@bk.ru>
2024-04-24 11:46:01 +0300

    7_10_JUnit5

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -6,9 +6,5 @@ import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
-public class JdbcMealServiceTest extends AbstractMealServiceTest {
-    @Override
-    @Ignore
-    public void createWithException() throws Exception {
-    }
+class JdbcMealServiceTest extends AbstractMealServiceTest {
 }
