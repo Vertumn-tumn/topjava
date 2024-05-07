4909b52a6dbf21c8acac41daa97a6945d360d193
evgen <vertumnartist@bk.ru>
2024-05-07 17:26:42 +0300

    questions

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,40 @@
+const mealAjaxUrl = "meals/";
+
+const ctx = {
+    ajaxUrl: mealAjaxUrl
+};
+
+$(function () {
+    makeEditable({
+        "columns": [
+            {
+                "data": "dateTime"
+            },
+            {
+                "data": "description"
+            },
+            {
+                "data": "calories"
+            },
+            {
+                "render": renderEditBtn,
+                "defaultContent": "",
+                "orderable": false
+            },
+            {
+                "render": renderDeleteBtn,
+                "defaultContent": "",
+                "orderable": false
+            }
+        ],
+        "order": [
+            [
+                0,
+                "desc"
+            ]
+        ]
+    });
+});
+
+
+
